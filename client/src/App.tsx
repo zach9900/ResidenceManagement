0551b59d3d236c42a6d40bda5deaee6741fa47be
yuvalmus <yuvalmus90@gmail.com>
2023-08-12 01:55:46 +0300

    Added Login page and created the overall design of the page including headers and image

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import "./App.css";
-import Floor from "./pages/Floor/Floor";
+import React from 'react'
+import './App.css'
+import Floor from './pages/Floor/Floor'
+import Login from './pages/Login/Login'
 
 function App() {
-  return (
-    <div className="App">
-      <Floor />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Login />
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ client/src/pages/Login/Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+	return (
+		<div className="container">
+			<div className="background-section"></div>
+			<div className="login-section">
+				<div className="form-container">
+					<h1 className="header">ברוכים השבים!</h1>
+					<h3 className="instructions">הקלד את האימייל והסיסמה כדי להתחבר</h3>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Login
