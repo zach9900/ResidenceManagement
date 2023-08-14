f3729c6f4e75f3b6d8f3a614ae8307f92d6a6b0a
yuvalmus <yuvalmus90@gmail.com>
2023-08-14 12:00:27 +0300

    alignment and positioning changes

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -8,12 +8,13 @@ import techni from "../../assets/bases/techni.png";
 import ramon from "../../assets/bases/ramon.png";
 
 function Login() {
-  const inputFields = ["אימייל", "סיסמה"];
-  const inputFieldsTypes = ["email", "password"];
+  const inputFields = ["מספר אישי", "סיסמה"];
+  const inputFieldsTypes = ["text", "password"];
   const basesImages = [hatserim, palmahim, telnof, techni, ramon];
   return (
     <div className="container">
-      <div className="background-section"></div>
+      <div className="background-section" />
+
       <div className="login-section">
         <div className="form-container">
           <h1 className="header">ברוכים השבים!</h1>
@@ -27,6 +28,7 @@ function Login() {
           ))}
           <button className="login-button">התחבר</button>
         </div>
+
         <div className="bases-container">
           {basesImages.map((baseImage, index) => (
             <img
