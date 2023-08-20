ad6e4c12b0d40b49f047d44e332e4268824ad54b
yuvalmus <yuvalmus90@gmail.com>
2023-08-20 13:14:51 +0300

    reorganize Login page and create login dto

---
+++ client/src/interfaces/loginUserDTO.interface.ts
@@ -0,0 +1,9 @@
+import Gender from "../constants/Gender.enum";
+import Role from "../constants/Role.enum";
+
+interface loginUserDTO {
+  personalNumber: string;
+  password: string;
+}
+
+export default loginUserDTO;
