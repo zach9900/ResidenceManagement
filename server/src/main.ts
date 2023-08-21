f8f48d2e389c146d512702015b5efaa4a71168c4
Nir Kalmanovitz <nir.kalmanovitz@gmail.com>
2023-08-21 21:36:06 +0300

    enable CORS in the application

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,6 +9,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN
+  });
+
   const options = new DocumentBuilder().setTitle('ResidenceManagement').build();
 
   const document = SwaggerModule.createDocument(app, options);
