17dbebe004530dabee8288eb57764df44a48ee34
Amit Leiba <amitleiba012@gmail.com>
2023-09-06 14:17:45 +0300

    change name of DTO and some spelling errors + add geographic location for the GetHilton DTO
    Co-authored-by: Nir Kalmanovitz <nir.kalmanovitz@gmail.com>
    Co-authored-by: Amit Leiba <amitleiba012@gmail.com>

--- server/src/hilton/models/get-hilton.dto.ts
+++ server/src/hilton/models/get-hilton.dto.ts
@@ -1,9 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { ApiSchema } from "@utils/decorators";
+import { GeoCoordinateDto } from "@utils/dtos";
 import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 @ApiSchema({ name: 'GetHilton' })
 export class GetHiltonDto {
+    @ApiProperty({
+        description: "The geographical coordinates of the Hilton",
+        example: {
+            latitude: 31.25,
+            longitude: 35.25
+        }
+    })
+    @IsNotEmpty()
+    geoCenter: GeoCoordinateDto;
+
     @ApiProperty({
         description: 'The number of the hilton you would like to get',
         example: 192
--- server/src/hilton/mvc/hilton.service.ts
+++ server/src/hilton/mvc/hilton.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
-import { Hilton } from '../models/hilton.schema';
+import { Hilton, HiltonDocument } from '../models/hilton.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { AddHiltonDto } from '@utils/add-hilton.dto';
 import { GeoCoordinateDto, GetHiltonDto } from '@utils/dtos';
+import { FloorService } from '@modules/floor.service';
+import { AddFloorDto } from '@utils/add-floor.dto';
+import { FloorDocument } from '@utils/floor.schema';
 
 @Injectable()
 export class HiltonService {
-    constructor(@InjectModel(Hilton.name) private readonly hiltonModel: Model<Hilton>) {}
+    constructor(
+        private readonly floorService: FloorService,
+        private readonly commanderService: CommanderService,
+        @InjectModel(Hilton.name) private readonly hiltonModel: Model<Hilton>,
+    ) {}
     
     async getHilton(hilton: GetHiltonDto) {
-        const wantedHilton = await this.hiltonModel.findOne({
-            buildingNumber: hilton.hiltonNumber,
+        const wantedHilton: HiltonDocument = await this.hiltonModel.findOne({
+            geoCenter: hilton.geoCenter,
         }).exec();
         
-        const hiltonWithFloors = (await wantedHilton.populate({
-            path: 'floors',
-            populate: {
-                path: 'rooms',
-            }
-        }));
-        
-        return hiltonWithFloors.floors.map((floor) => floor.rooms.map((room) => { room.bedsNumber, room.freeBeds, room.roomNumber }));
+        return this.floorService.getFloors(wantedHilton);
     }
     
     async createHilton(hilton: AddHiltonDto) {
@@ -30,24 +30,25 @@ export class HiltonService {
             buildingNumber: hilton.buildingNumber,
             geoCenter: hilton.geoCenter
         });
+
         return await newHilton.save();
     }
     
-    async updateHiltonWithFloors(buildingLocation: GeoCoordinateDto, floors: FloorDto[]) {
+    async updateHiltonWithFloors(buildingLocation: GeoCoordinateDto, floors: AddFloorDto[]) {
         const hiltonToUpdate = await this.hiltonModel.findOne(
             { geoCenter: buildingLocation }
         ).exec();
         
         if (hiltonToUpdate !== undefined) return null;
 
-        for (const floor in floors) {
+        for (const floor of floors) {
             const savedFloor = await this.floorService.createFloor(floor);
             hiltonToUpdate.floors.push(savedFloor._id);
         }
         return await hiltonToUpdate.save();
     }
         
-    async updateHiltonWithManager(buildingLocation: GeoCoordinateDto, buildingManager: BuildingMaanagerDto) {
+    async updateHiltonWithManager(buildingLocation: GeoCoordinateDto, buildingManager: BuildingManagerDto) {
         const hiltonToUpdate = await this.hiltonModel.findOne(
             { geoCenter: buildingLocation }
         ).exec();
