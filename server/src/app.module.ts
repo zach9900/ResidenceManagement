b63c27bc4b942c126d941b6fa6aa4c6188ff6b80
Nir Kalmanovitz <nir.kalmanovitz@gmail.com>
2023-09-09 11:23:52 +0300

    change the swagger UI

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CourseCommanderModule } from './courseCommander/mvc/courseCommander.module';
 import { BaseModule } from '@modules/base.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     CourseCommanderModule,
     BaseModule,
     MongooseModule.forRoot(process.env.DB_PATH),
