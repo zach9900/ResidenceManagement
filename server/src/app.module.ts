59df327c3cc685683ba212324127faeabd9a605a
Nir Kalmanovitz <nir.kalmanovitz@gmail.com>
2023-09-11 15:49:29 +0300

    add all the modules to the app

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,14 +3,26 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CourseCommanderModule } from './courseCommander/mvc/courseCommander.module';
 import { BaseModule } from '@modules/base.module';
 import { ConfigModule } from '@nestjs/config';
+import { CommanderModule } from './commander/mvc/commander.module';
+import { FloorModule } from '@modules/floor.module';
+import { HiltonModule } from '@modules/hilton.module';
+import { ManagerModule } from './manager/mvc/manager.module';
+import { RoomModule } from './room';
+import { SoldierModule } from './soldier';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    CourseCommanderModule,
     BaseModule,
+    CommanderModule,
+    CourseCommanderModule,
+    FloorModule,
+    HiltonModule,
+    ManagerModule,
+    RoomModule,
+    SoldierModule,
     MongooseModule.forRoot(process.env.DB_PATH),
   ],
 })
