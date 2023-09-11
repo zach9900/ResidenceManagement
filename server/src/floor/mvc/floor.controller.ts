93a5eb1697a7bef4ad28d7ab6402d0179d3f0a51
Nir Kalmanovitz <nir.kalmanovitz@gmail.com>
2023-09-11 15:43:26 +0300

    add functions for the hilton controller

--- server/src/floor/mvc/floor.controller.ts
+++ server/src/floor/mvc/floor.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get, Patch, Body, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { FloorService } from './floor.service';
+import { GetFloorDto } from '@utils/get-floor.dto';
+import { AddFloorDto } from '@utils/add-floor.dto';
+import { GeoCoordinateDto } from '@utils/dtos';
+import { FloorManagerDto } from '@utils/floor-manager.dto';
 
 @ApiTags('Floor')
 @Controller('floor')
@@ -8,17 +12,12 @@ export class FloorController {
     constructor(private readonly floorService: FloorService) {}
 
     @Get()
-    async readFloor() {
-        
-    }
-
-    @Post()
-    async createFloor() {
-
+    async readFloor(@Body() floor: GetFloorDto) {
+        return await this.floorService.getFloor(floor);
     }
 
     @Patch()
-    async updateFloorWithManager() {
-
+    async updateFloorWithManager(@Body() floorLocation: GeoCoordinateDto, @Body() floorManager: FloorManagerDto) {
+        return await this.floorService.updateFloorWithManager(floorLocation, floorManager);
     }
 }
